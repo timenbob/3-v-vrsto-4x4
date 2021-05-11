3fc5d150121d8b8fb47411063c628fa86445a049
timenbob <timen.bobnar@gmail.com>
2021-05-11 10:05:36 +0200

    dodan class Tri_vrsto_štiri pricetek spletnega vmesnika

--- model.py
+++ model.py
@@ -113,3 +113,98 @@ class Igrica:
             self.igralec = "O"
         elif self.igralec == "O":
             self.igralec = "X"
+
+
+
+
+class Tri_vrsto_štiri:
+    def __init__(self, igralec='X'):
+        self.igralec = igralec
+        self.plosca = ["-", "-", "-", "-",
+                       "-", "-", "-", "-",
+                       "-", "-", "-", "-",
+                       "-", "-", "-", "-"]
+        self.sporocilo = ""
+        self.konec = ""
+
+    def nova_igra(self):
+        self.igralec = "X"
+        self.plosca = ["-", "-", "-", "-",
+                       "-", "-", "-", "-",
+                       "-", "-", "-", "-",
+                       "-", "-", "-", "-"]
+        self.sporocilo = ""
+        self.konec = ""
+
+    def menjava_igralcev(self):
+        if self.igralec == "X":
+            self.igralec = "O"
+        elif self.igralec == "O":
+            self.igralec = "X"
+
+    def zmaga(self):
+        # 2 različni na vrstico
+        if self.plosca[0] == self.plosca[1] == self.plosca[2] != '-':
+            return self.igralec
+        elif self.plosca[1] == self.plosca[2] == self.plosca[3] != '-':
+            return self.igralec
+        elif self.plosca[4] == self.plosca[5] == self.plosca[6] != '-':
+            return self.igralec
+        elif self.plosca[5] == self.plosca[6] == self.plosca[7] != '-':
+            return self.igralec
+        elif self.plosca[8] == self.plosca[9] == self.plosca[10] != '-':
+            return self.igralec
+        elif self.plosca[9] == self.plosca[10] == self.plosca[11] != '-':
+            return self.igralec
+        elif self.plosca[12] == self.plosca[13] == self.plosca[14] != '-':
+            return self.igralec
+        elif self.plosca[13] == self.plosca[14] == self.plosca[15] != '-':
+            return self.igralec
+        # stolpci
+        elif self.plosca[0] == self.plosca[4] == self.plosca[8] != '-':
+            return self.igralec
+        elif self.plosca[4] == self.plosca[8] == self.plosca[12] != '-':
+            return self.igralec
+        elif self.plosca[1] == self.plosca[5] == self.plosca[9] != '-':
+            return self.igralec
+        elif self.plosca[51] == self.plosca[9] == self.plosca[13] != '-':
+            return self.igralec
+        elif self.plosca[2] == self.plosca[6] == self.plosca[10] != '-':
+            return self.igralec
+        elif self.plosca[6] == self.plosca[10] == self.plosca[14] != '-':
+            return self.igralec
+        elif self.plosca[3] == self.plosca[7] == self.plosca[11] != '-':
+            return self.igralec
+        elif self.plosca[7] == self.plosca[11] == self.plosca[15] != '-':
+            return self.igralec
+        # diagonale
+        elif self.plosca[8] == self.plosca[5] == self.plosca[2] != '-':
+            return self.igralec
+        elif self.plosca[1] == self.plosca[6] == self.plosca[11] != '-':
+            return self.igralec
+        elif self.plosca[4] == self.plosca[9] == self.plosca[14] != '-':
+            return self.igralec
+        elif self.plosca[13] == self.plosca[10] == self.plosca[7] != '-':
+            return self.igralec
+        elif self.plosca[12] == self.plosca[9] == self.plosca[6] != '-':
+            return self.igralec
+        elif self.plosca[9] == self.plosca[6] == self.plosca[3] != '-':
+            return self.igralec
+        return False
+
+    def napacna_poteza(self, polje):
+        if polje not in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]:
+            print("Nepravilen vnos")
+            return True
+        return False
+
+    def ponovljena_poteza(self, polje):
+        if self.plosca[polje] != '-':
+            print("Polje je že zasedeno")
+            return True
+        return False
+
+    def neodloceno(self):
+        if '-' not in self.plosca:
+            return True
+        return False
